8e291ab6a6c85e765f45eec54f961a59f860a9bc
Tu Diem <tudiem0104199@gmail.com>
2022-09-15 08:20:16 +0700

    init project

---
+++ Exercise/Database/Implements/ProductService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise.Database.Implements
+{
+    public abstract class ProductService : DbConnection
+    {
+        private DapperConnection _dapperConnection;
+        private IDbConnection _connect;
+
+        public ProductService()
+        {
+            _dapperConnection = new DapperConnection();
+            _connect = _dapperConnection.GetDbConnection();
+        }
+
+        public override string ConnectionString { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public override string Database => throw new NotImplementedException();
+
+        public override string DataSource => throw new NotImplementedException();
+
+        public override string ServerVersion => throw new NotImplementedException();
+
+        public override ConnectionState State => throw new NotImplementedException();
+
+        public override void ChangeDatabase(string databaseName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Close()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Open()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override DbTransaction BeginDbTransaction(IsolationLevel isolationLevel)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override DbCommand CreateDbCommand()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
